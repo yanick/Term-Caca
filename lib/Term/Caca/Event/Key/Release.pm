72d64a69f21e7c83d2ed8098596027e89763a405
Yanick Champoux <yanick@babyl.dyndns.org>
2011-09-25 13:46:44 -0500

    document events

--- lib/Term/Caca/Event/Key/Release.pm
+++ lib/Term/Caca/Event/Key/Release.pm
@@ -1,6 +1,25 @@
 package Term::Caca::Event::Key::Release;
 # abstract: event triggered by a key release
 
+=head1 SYNOPSIS
+
+    use Term::Caca qw/ :events /;
+
+    my $t = Term::Caca->new;
+    while ( 1 ) {
+        my $event = $t->wait_for_event( 
+            mask => $KEY_RELEASE,
+        );  
+        
+        print "character typed: ", $event->char;
+    }
+
+=head1 DESCRIPTION
+
+Generated when a key is released.
+
+=cut
+
 use strict;
 use warnings;
 
@@ -11,6 +30,16 @@ sub new {
     my $self = Term::Caca::Event::Key->new( @_ );
     return bless $self, $class;
 }
+
+=method char()
+
+Returns the character released.
+
+=head1 SEE ALSO
+
+L<Term::Caca::Event::Key>, L<Term::Caca::Event::Key::Press>
+
+=cut
 
 1;
 
