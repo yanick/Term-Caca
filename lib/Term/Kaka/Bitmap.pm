fe697e17774ef5f129c236d7de0ba7b4723c44b6
Yanick Champoux <yanick@babyl.dyndns.org>
2013-03-02 14:25:50 -0500

    remove Term::Kaka::*

--- lib/Term/Kaka/Bitmap.pm
+++
@@ -1,8 +0,0 @@
-package Term::Kaka::Bitmap;
-#ABSTRACT: Term::Caca::Bitmap
-
-use strict;
-use base 'Term::Caca::Bitmap';
-1;
-
-__END__
--- lib/Term/Kaka/Sprite.pm
+++
@@ -1,8 +0,0 @@
-package Term::Kaka::Sprite;
-#ABSTRACT:  Term::Caca::Sprite
-
-use strict;
-use base 'Term::Caca::Sprite';
-1;
-
-__END__
